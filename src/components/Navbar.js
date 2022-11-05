3f5f154166fac37787fc0bbf3a701c33c33dbd2c
Andyshix <81393575+Andyshix@users.noreply.github.com>
2022-11-05 06:01:31 -0400

    Barras de navegacion actualizado(Andrea)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,21 +12,41 @@ function Navbar() {
     const toogleNavbar = () => {
         setOpenLinks(!openLinks);
     }
+    
+    const test = () => { 
+        let aux = document.getElementById("cerrarsesion")
+        let pos = document.getElementById("iniciarsesion")
+        let aux2 = document.getElementById("cerrarsesion2")
+        let pos2 = document.getElementById("iniciarsesion2")
+        aux.style.display="flex";
+        aux2.style.display="flex";
+        pos.style.display="none" ;
+        pos2.style.display="none" ;
+    }
+        
+    const test2 = () => { 
+        let aux = document.getElementById("cerrarsesion")
+        let pos = document.getElementById("iniciarsesion")
+        let aux2 = document.getElementById("cerrarsesion2")
+        let pos2 = document.getElementById("iniciarsesion2")
+        aux.style.display="none";
+        aux2.style.display="none";
+        pos.style.display="flex" ;
+        pos2.style.display="flex" ;
+    }
 
   return (
     <div className="navbar">
         <div className="leftSide" id={openLinks ? "open" : "close"}>
             <img src={Logo}/>
-            <div className="hiddenLinks">
-            <Link to="/">Home</Link>
-            <Link to="/registrarPromocion">Registrar Negocio</Link>
-            <Link to="/registrarProducto">Registrar Promocion</Link>                 
-            </div>
+          
         </div>
         <div className="rightSide">
-            <Link to="/">Home</Link>
-            <Link to="/registrarNegocio">Registrar Negocio</Link>
-            <Link to="/registrarPromocion">Registrar Promocion</Link>
+            <Link to="/registrarNegocio"id="iniciarsesion">Registrar Negocio</Link>
+            <Link to="/" id="iniciarsesion2" onClick={test}>Iniciar sesion</Link>
+            <Link to="/registrarPromocion"id="cerrarsesion">Registrar Promocion</Link>
+            <Link to="/"id="cerrarsesion2"onClick={test2}>Cerrar Sesion</Link>
+            
             <button onClick={toogleNavbar}>
                 <ReorderIcon/>
             </button>
