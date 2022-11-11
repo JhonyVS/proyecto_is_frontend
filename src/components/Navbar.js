fd799ebe4c8bbb4362ec375bbc76dc353f2225a0
Carlos Daniel <carlosjuchasara@gmailcom>
2022-11-11 01:38:17 -0400

    El logo redirecciona a la pantalla principal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,11 +16,11 @@ function Navbar() {
   return (
     <div className="navbar">
         <div className="leftSide" id={openLinks ? "open" : "close"}>
-            <img src={Logo}/>
+            <a href='/'><img src={Logo} class="logo"/></a>
             <div className="hiddenLinks">
             <Link to="/">Home</Link>
             <Link to="/registrarPromocion">Registrar Negocio</Link>
-            <Link to="/registrarProducto">Registrar Promocion</Link>                 
+            <Link to="/registrarProducto">Registrar Promoción</Link>                 
             </div>
         </div>
         <div className="rightSide">
