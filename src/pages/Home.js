3552a797bd5f1b8745ebee96b68d0c2a26a7449b
kw3a <torrezaldair777@gmail.com>
2022-11-10 23:51:52 -0400

    imagen de fondo no centrada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,35 +6,37 @@ import imagenFondo from '../assets/fondoComida.png';
 import '../styles/Home.css';
 
 function Home() {
-  return (<div className = "home" style={{ backgroundImage: `url(${imagenFondo})`}}>
-    <Container>
-    
-        <div className='headerContainer' >
-            <Row>
-                <Col>
-                <h1 >Lo mejor en Promociones</h1>
-                </Col>
-            </Row>
-            <Row>
-                <p>Solo promociones</p>
-                <a href='/promociones'>
-                    <button>VER PROMOCIONES</button>
-                </a>
-            </Row>
-            {/* <Grid item sx= {6} sm ={6} md = {12}>
-            <h1 >Lo mejor en Promociones
-            </h1>
+  return (
+    <Container className='headerContainer'>
+        <div className = "home" style={{ backgroundImage: `url(${imagenFondo})`}}>
+            {/* <div className='headerContainer' > */}
+                <Row>
+                    <Col sx = {6} sm = {6} md = {12}>
+                    <h1 >Lo mejor en Promociones</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <p>Solo promociones</p>
+                </Row>
+                <Row>
+                    <a href='/promociones'>
+                        <button>VER PROMOCIONES</button>
+                    </a>
+                </Row>
+                {/* <Grid item sx= {6} sm ={6} md = {12}>
+                <h1 >Lo mejor en Promociones
+                </h1>
 
-            </Grid>
-            
-            <p>Solo promociones</p>
-            <Link to="/promociones">
-                <button>VER PROMOCIONES</button> 
-            </Link> */}
+                </Grid>
+                
+                <p>Solo promociones</p>
+                <Link to="/promociones">
+                    <button>VER PROMOCIONES</button> 
+                </Link> */}
 
         </div>
     </Container>
-    </div>
+    // </div>
   )
 }
 
