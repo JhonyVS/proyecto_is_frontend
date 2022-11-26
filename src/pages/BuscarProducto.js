f0be774d6bf0082ec2cdb6009174f5712e291a38
Rustazaga <nevermorewars@gmail.com>
2022-11-25 22:01:58 -0400

    algo

---
+++ src/pages/BuscarProducto.js
@@ -0,0 +1,52 @@
+import React, {useEffect,useState} from 'react'
+import {MenuList} from '../helpers/MenuList'
+import PromoItem from '../components/PromoItem'
+import '../styles/Promociones.css'
+import AlitasMostaza from '../assets/promociones/alitasMostasa.jpg';
+
+
+const baseUrl = `${process.env.React_APP_API}/api/destacados`;
+
+function Promociones() {
+
+  const[products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchData();
+
+  },[])
+
+  const fetchData = async () => {
+    await fetch(baseUrl)
+    .then((res) => res.json())
+    .then((data) => setProducts(data))
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  console.log(products)
+
+
+  return (
+    <div className='menu'>
+        <h1 className='menuTile'>Productos </h1>
+        <div className="menuList">
+            {MenuList.map((promoItem, key) => {
+                return(
+                  products.map((product) => (
+                    <PromoItem 
+                          key={product.producto_id} 
+                          image = {AlitasMostaza}
+                          name={product.nombre} 
+                          precio={product.precio} 
+                          precioDescuento={product.descuento} />
+                  ))
+                );
+
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Promociones
