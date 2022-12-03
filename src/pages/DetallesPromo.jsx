24799d048494bd1d62a7685c62c18f5e822775b7
Andyshix <you@example.com>
2022-12-03 01:40:45 -0400

    detalles Promo id static

--- src/pages/DetallesPromo.jsx
+++ src/pages/DetallesPromo.jsx
@@ -7,8 +7,8 @@ import { useEffect  , useState} from 'react'
 
 
 
-function DetallesPromo(idPromo) {
-    console.log(idPromo)
+function DetallesPromo() {
+    const idPromo=1
     const baseUrl = `${process.env.React_APP_API}/api/producto/${idPromo}`
     
     const [productos, setProductos]= useState([]);
