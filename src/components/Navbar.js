cfc8c4ca0d94a31206d081da915a155f17224ae1
David Gregori Rodriguez Calle <115518033+Perzival5@users.noreply.github.com>
2022-12-03 13:14:40 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Logo from '../assets/logo.png';
-import {Link} from 'react-router-dom';
-import '../styles/Navbar.css'
-import ReorderIcon from '@mui/icons-material/Reorder';
-
-
+import '../styles/Navbar.css';
+import Cookies from 'universal-cookie';
+import { Link } from 'react-router-dom';
+const cookies = new Cookies();
+// import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
-    const [openLinks, setOpenLinks] = useState(false);
+    
 
     const toogleNavbar = () => {
-        setOpenLinks(!openLinks);
+        window.location.href='./';
     }
-    
-    const test = () => { 
-        let aux = document.getElementById("cerrarsesion")
-        let pos = document.getElementById("iniciarsesion")
-        let aux2 = document.getElementById("cerrarsesion2")
-        let pos2 = document.getElementById("iniciarsesion2")
-        aux.style.display="flex";
-        aux2.style.display="flex";
-        pos.style.display="none" ;
-        pos2.style.display="none" ;
-    }
-        
+
     const test2 = () => { 
         let aux = document.getElementById("cerrarsesion")
         let pos = document.getElementById("iniciarsesion")
@@ -33,34 +22,27 @@ function Navbar() {
         aux2.style.display="none";
         pos.style.display="flex" ;
         pos2.style.display="flex" ;
+        cookies.remove('id', {path: "/"});
+        cookies.remove('token', {path: "/"});
+        window.location.href='./';
     }
 
   return (
     <div className="navbar">
-        <div className="leftSide" id={openLinks ? "open" : "close"}>
-            <img src={Logo}/>
-          
-            <a href='/'><img src={Logo} class="logo"/></a>
-            <div className="hiddenLinks">
-            <Link to="/">Home</Link>
-            <Link to="/registrarPromocion">Registrar Negocio</Link>
-            <Link to="/registrarProducto">Registrar Promoción</Link>                 
-            </div>
+        {/* <a className="text-white" href='/registrarNegocio'>fasdfsdafsdf</a> */}
+        <div className="leftSide" >
+            <img src={Logo}  className='logo' onClick={toogleNavbar}/>
         </div>
         <div className="rightSide">
             <Link to="/">Home</Link>
             <Link to="/registrarNegocio"id="iniciarsesion">Registrar Negocio</Link>
-            <Link to="/" id="iniciarsesion2" onClick={test}>Iniciar sesion</Link>
+            <Link to="/ini" id="iniciarsesion2" >Iniciar sesion</Link>
             <Link to="/registrarPromocion"id="cerrarsesion">Registrar Promocion</Link>
             <Link to="/"id="cerrarsesion2"onClick={test2}>Cerrar Sesion</Link>
             
-            <button onClick={toogleNavbar}>
-                <ReorderIcon/>
-            </button>
 
         </div>
     </div>
   )
 }
-
 export default Navbar
